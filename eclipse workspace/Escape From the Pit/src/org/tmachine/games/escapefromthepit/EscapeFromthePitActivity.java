616441d23a7faa5aec9c49c1af38e7a61038ec11
adam <git.sucks.this.email.doesnt.exist@mailinator.com>
2011-08-21 14:50:27 +0100

    Created eclipse workspace, added personal reference to local git of related project (entity systems - already up on github)

---
+++ eclipse workspace/Escape From the Pit/src/org/tmachine/games/escapefromthepit/EscapeFromthePitActivity.java
@@ -0,0 +1,13 @@
+package org.tmachine.games.escapefromthepit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EscapeFromthePitActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

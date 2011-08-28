7ee76a309965d3ca24924fb80ae02c9a7c8462f9
adam <git.sucks.this.email.doesnt.exist@mailinator.com>
2011-08-28 02:58:34 +0100

    Improved touch processing to do the heavy stuff ONLY once per game-tick, and to also re-use the onscreen display entity rather than destroy + recreate every game-tick

--- eclipse workspace/Escape From the Pit/src/org/tmachine/games/escapefromthepit/SubsystemTouchHandler.java
+++ eclipse workspace/Escape From the Pit/src/org/tmachine/games/escapefromthepit/SubsystemTouchHandler.java
@@ -9,6 +9,7 @@ import android.util.*;
 import android.view.*;
 
 import com.wikidot.entitysystems.rdbmsbeta.*;
+import com.wikidot.entitysystems.rdbmsbeta.examplecomponents.*;
 
 public class SubsystemTouchHandler implements SubSystem
 {
@@ -16,6 +17,7 @@ public class SubsystemTouchHandler implements SubSystem
 	EntityManager em;
 	
 	float lastX, lastY;
+	MetaEntity onScreenArrowIndicator;
 	
 	@Override
 	public void processOneGameTick(long lastFrameTime)
@@ -24,9 +26,72 @@ public class SubsystemTouchHandler implements SubSystem
 		{
 			//Log.i(getClass().getSimpleName(), "Found "+pendingMotionEvents.size()+" pending touch events" );
 			
+			/**
+			 * Store the "last" values because they will be destroyed by the next step
+			 */
+			float initialX = lastX;
+			float initialY = lastY;
+			
+			/**
+			 * Process ALL events that came in since last game-tick,
+			 * and allow them to update the "last" vars
+			 */
 			for( FixedMotionEvent event : pendingMotionEvents )
 			{
 				processNextEvent( event );
+			}
+			
+			/**
+			 * Finally, look at the net resulting change in X / Y, and react accordingly
+			 */
+			if( onScreenArrowIndicator != null )
+			{
+				MetaEntity player = MetaEntity.loadFromEntityManager( em.getAllEntitiesPossessingComponent(CPlayer.class).iterator().next() );
+				float thresholdForIgnoringInput = 2;
+				if( Math.abs(initialX-lastX) > Math.abs(initialY - lastY) )
+				{
+					if( Math.abs(initialX-lastX) > thresholdForIgnoringInput )
+						if( initialX > lastX )
+						{
+							CPosition pos = player.get(CPosition.class);
+							pos.x -= 10;
+							player.get(CAndroidDrawable.class).resourceID = R.drawable.personleft;
+							//"left arrow"
+							onScreenArrowIndicator.get( CAndroidDrawable.class ).resourceID = ( R.drawable.arrowleft);
+							onScreenArrowIndicator.get( CTouch.class ).value = TouchType.LEFT;
+						}
+						else
+						{
+							CPosition pos = player.get(CPosition.class);
+							pos.x += 10;
+							player.get(CAndroidDrawable.class).resourceID = R.drawable.personright;
+							onScreenArrowIndicator.get( CAndroidDrawable.class ).resourceID = ( R.drawable.arrowright);
+							onScreenArrowIndicator.get( CTouch.class ).value = TouchType.RIGHT;
+						}
+				}
+				else if( Math.abs(initialX-lastX) < Math.abs(initialY - lastY) )
+				{
+					if( Math.abs(initialY-lastY) > thresholdForIgnoringInput )
+						if( initialY > lastY )
+						{	
+							CPosition pos = player.get(CPosition.class);
+							pos.y -= 10;
+							onScreenArrowIndicator.get( CAndroidDrawable.class ).resourceID = ( R.drawable.arrowup);
+							onScreenArrowIndicator.get( CTouch.class ).value = TouchType.UP;
+						}
+						else
+						{
+							CPosition pos = player.get(CPosition.class);
+							pos.y += 10;
+							onScreenArrowIndicator.get( CAndroidDrawable.class ).resourceID = ( R.drawable.arrowdown );
+							onScreenArrowIndicator.get( CTouch.class ).value = TouchType.DOWN;
+						}
+				}
+				else
+				{
+					onScreenArrowIndicator.get( CAndroidDrawable.class ).resourceID = ( R.drawable.arrowdot);
+					onScreenArrowIndicator.get( CTouch.class ).value = TouchType.NONE;
+				}
 			}
 
 			pendingMotionEvents.clear();
@@ -43,46 +108,19 @@ public class SubsystemTouchHandler implements SubSystem
 	protected void touch_start( float x, float y )
 	{
 		//Log.i(getClass()+"", "touch_start: adding a dot arrow");
-		new MetaEntity( "dot arrow initial", new Position( 300, 300, 100, 100), new CAndroidDrawable( R.drawable.arrowdot), new CTouch( TouchType.NONE ) );
+		onScreenArrowIndicator = new MetaEntity( "dot arrow initial", new CPosition( 300, 300, 100, 100), new CAndroidDrawable( R.drawable.arrowdot), new CTouch( TouchType.NONE ) );
 	}
 	
 	protected void touch_move( float x, float y )
 	{
-		Log.i( getClass().getSimpleName(), "touch_move... ("+x+","+y+")");
-		for( UUID entity : em.getAllEntitiesPossessingComponent( CTouch.class ) )
-		{
-			Log.i("", "Removing entity that has CTOUCH: "+entity);
-			em.killEntity(entity);
-			Log.i("", "...removed entity that has CTOUCH: "+entity);
-		}
-		
-		if( Math.abs(lastX-x) > Math.abs(lastY - y) )
-		{
-			if( lastX > x )
-				new MetaEntity( "left arrow", new Position( 300, 300, 100, 100), new CAndroidDrawable( R.drawable.arrowleft), new CTouch( TouchType.LEFT ) );
-			else
-				new MetaEntity( "right arrow", new Position( 300, 300, 100, 100), new CAndroidDrawable( R.drawable.arrowright), new CTouch( TouchType.RIGHT ) );
-		}
-		else if( Math.abs(lastX-x) < Math.abs(lastY - y) )
-		{
-			if( lastY > y )
-				new MetaEntity( "up arrow", new Position( 300, 300, 100, 100), new CAndroidDrawable( R.drawable.arrowup), new CTouch( TouchType.UP ) );
-			else
-				new MetaEntity( "down arrow", new Position( 300, 300, 100, 100), new CAndroidDrawable( R.drawable.arrowdown), new CTouch( TouchType.DOWN ) );
-		}
-		else
-			new MetaEntity( "dot arrow transitory", new Position( 300, 300, 100, 100), new CAndroidDrawable( R.drawable.arrowdot), new CTouch( TouchType.NONE ) );
+		//Log.i( getClass().getSimpleName(), "touch_move... ("+x+","+y+")");
 		
 	}
 	
 	protected void touch_up( float x, float y )
 	{
-		for( UUID entity : em.getAllEntitiesPossessingComponent( CTouch.class ) )
-		{
-			Log.i("", "Removing entity that has CTOUCH: "+entity);
-			em.killEntity(entity);
-			Log.i("", "...removed entity that has CTOUCH: "+entity);
-		}
+		onScreenArrowIndicator.kill();
+		onScreenArrowIndicator = null;		
 	}
 	
 	
